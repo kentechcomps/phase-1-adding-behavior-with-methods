0be3edfbe7cbed34b5af637e0fb6390ca2dfeb6b
user <mutukukennedy34@gmail.com>
2023-07-12 17:23:06 +0300

    behavior

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+class Cat{
+ constructor(aname , sex){
+    this.aname = aname;
+    this.sex = sex;
+ }
+ speak(){
+    console.log(`${this.aname} says meow!`);
+ }
+}
+class Dog{
+    constructor(aname , sex){
+        this.aname = aname;
+        this.sex = sex;
+    }
+    speak(){
+        console.log(`${this.aname} always barks`);
+    }
+}
+class Bird{
+    constructor(aname , sex){
+        this.aname = aname;
+        this.sex = sex;
+    }
+    speak(){
+        if(this.sex === male){
+            console.log(`${this.aname} this is the parrot`);
+        }else{
+            console.log(`${this.aname} i squawk`);
+        }
+       
+    }
+
+}
